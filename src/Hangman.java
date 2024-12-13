d0f23aef85097f5f60b45cab46ee3d084a575485
theosMketsu <156222919+theosMketsu@users.noreply.github.com>
2024-12-13 19:32:10 +0200

    replay error

--- src/Hangman.java
+++ src/Hangman.java
@@ -7,22 +7,24 @@ import java.util.Scanner;
 public class Hangman {
     
     static boolean gameOver;
-    static int guesses = 6;
+    static int guesses;
 
     static boolean guessNumber(int num, int val) {
 
         if (val == num) return true;
 
+        //val is your guess
+        guesses--;
         if (val > num) {
-            guesses--;
-            if ((val/num) > 3) {
+            // Guess is bigger
+            if ((3*num) < val) {
                 System.out.println("Guess too Large");
             } else {
                 System.out.println("Try a smaller guess");
             }
-        } else if (num != 0 && num < val) {
-            guesses--;
-            if ((num/val) > 3) {
+        } else if (val < num) {
+            // Guess is smaller
+            if ((3*val) < num) {
                 System.out.println("Guess too Small");
             } else {
                 System.out.println("Try a bigger number");
@@ -30,6 +32,26 @@ public class Hangman {
         }
 
         return false;
+    }
+
+    static void guessWord(char ch, String word, boolean[] wordGuess) {
+
+        int j;
+        int wordLen;
+
+        j = word.indexOf(ch); //Get index of character
+        if (j == -1) {
+            // if guess is incorrect
+            guesses--;
+        } else {
+            wordLen = word.length();
+            // Check if the guessed character in other indexes
+            for (int i = j; i < wordLen; i++) {
+                if (ch == word.charAt(i)) {
+                    wordGuess[i] = true;
+                }
+            }
+        }
     }
 
     static void printWord(String word, boolean[] wordGuess) {
@@ -51,14 +73,17 @@ public class Hangman {
                 System.out.printf("%c ", c);
             }
         }
+
         System.out.println();
     }
     public static void main(String[] args) throws IOException {
     
         // 0. Set Gui to true(1) or false(0) in terminal
+        boolean[] wordGuess = new boolean[32];
         char ch;
-        int gui = 1; //gui default is true
+        int gui; //gui default is true
         int num = 0;
+        int val;
         int wordLen;
         Random rand = new Random();
         String word = "";
@@ -78,62 +103,52 @@ public class Hangman {
         Scanner sc = new Scanner(System.in);
         while (game.equals("")) {
             // Ensure that the correct option is selected
-            game = sc.next();
+            game = sc.next().toUpperCase();
             if (game.equals("W")) {
                 System.out.println("Guess the Word");
+                // guess word selection
+                //word = getWord(word, rand).toLowerCase();
+                word = "Hello".toLowerCase();
+                wordLen = word.length();
+                wordGuess = new boolean[wordLen];
+                printWord(word,wordGuess);
             } else if (game.equals("N")) {
                 System.out.println("Guess the Number");
+                num = rand.nextInt(100);
             } else {
                 System.err.println("Invalid option, Try again");
                 game = "";
             }
+
         }
-
-
-        // guess word selection
-        word = getWord(word, rand).toLowerCase();
-        wordLen = word.length();
-        boolean[] wordGuess = new boolean[wordLen];
         
-        int j; // index of character player guessed
-        int guesses = 6; // Num of incorrect guesses
-        int val;
+        guesses = 6;
         //Play Game
         while (!gameOver) {
-
-            if (game.equals("W")) {
-                printWord(word,wordGuess);
-            } else {
-                num = rand.nextInt();
-            }
-
+            System.out.printf("Guesses: %d\n", guesses);
             System.out.println("Enter a guess");
+            printWord(word,wordGuess);
             guess = sc.next().toLowerCase();
+            System.out.println();
 
             // guess Number game
             if (game.equals("N")) {
                 try {
                     val = Integer.parseInt(guess);
+                    gameOver = guessNumber(num, val);
                 } catch (Exception e) {
                     System.err.println("Enter a number");
                     continue;
                 }
-                guessNumber(num, val);
-            }
-
-            // ---word guess---
-            ch = guess.charAt(0); //only read the first character
-            j = word.indexOf(ch); //Get index of character
-
-            if (j == -1) {
-                // if guess is incorrect
-                guesses--;
+                
             } else {
-
-                // Check if the guessed character in other indexes
-                for (int i = j; i < wordLen; i++) {
-                    if (ch == word.charAt(i)) {
-                        wordGuess[i] = true;
+                // ---word guess---
+                ch = guess.charAt(0); //only read the first character
+                guessWord(ch, word, wordGuess);
+                for (boolean x: wordGuess) {
+                    gameOver = x;
+                    if (!gameOver) {
+                        break;
                     }
                 }
             }
@@ -142,31 +157,58 @@ public class Hangman {
             if (guesses == 0) {
                 // Restart Game or End Game
                 System.out.println("YOU LOSE");
+                if (game.equals("W")) {
+                    System.out.printf("Word is %s\n\n", word);
+                } else {
+                    System.out.printf("Number is %d\n\n", num);
+                }
+                guesses = -1;
+                gameOver = true;
+
+            } else if (gameOver) {
+                System.out.println("You WIN");
+                if (game.equals("W")) {
+                    System.out.printf("Word is %s\n\n", word);
+                } else {
+                    System.out.printf("Number is %d\n\n", num);
+                }
+                guesses = -1;
+            }
+
+            if (guesses == -1) {
                 System.out.print("Play again?\n yes(y)\tno(n)\n");
                 ch = sc.next().toLowerCase().charAt(0);
-
-                gameOver = true;
                 if (ch == 'y') {
                     guesses = 6; // Reset number of guesses
                     gameOver = false; // reset gameover
                     System.out.println("Select different Game: yes(y)\tno(n)");
                     ch = sc.next().toLowerCase().charAt(0);
-                    if (ch == 'y' && game.equals("W")) {
-                        game = "N";
-                    } else if (ch == 'y' && game.equals("N")) {
-                        game = "W";
+                    if (ch == 'y') {
+                        if (game.equals("W")) {
+                            System.out.println("Guess the Number");
+                            game = "N";
+                        } else if (ch == 'y' && game.equals("N")) {
+                            System.out.println("Guess the Word");
+                            game = "W";
+                        }
                     }
 
+                    System.out.println(game);
                     if (game.equals("W")) {
                         // Restart the Game
                         word = getWord(word, rand); // New Word
                         wordLen = word.length();
                         wordGuess = new boolean[wordLen];
+                        printWord(word,wordGuess);
                     } else {
-                        num = rand.nextInt();
+                        num = rand.nextInt(100);
                     }
                 }
             }
+
+        }
+
+        if (guesses > 0) {
 
         }
         sc.close();
@@ -174,10 +216,8 @@ public class Hangman {
 
     static String getWord(String word, Random rand) throws IOException {
         Path filePath = Path.of("../Wordlists/english.txt");
-
         // Get all lines as a List of Strings
         var lines = Files.readAllLines(filePath);
-
         // Pick a random word from the list
         int guess = rand.nextInt(lines.size());
         word = lines.get(guess);
